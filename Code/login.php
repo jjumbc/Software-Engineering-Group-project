0831315094a9473f1b018508000cda53b528f3e6
Justus <actionjackson0@gmail.com>
2016-03-28 23:15:51 -0400

    Uploaded login page, logo, background, and favicon

---
+++ Code/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<title>Hello!</title>
+		<link rel="stylesheet" type="text/css" href="styles.css">
+		<link rel="icon" href="fav.png" type="image/png" sizes="16x16">
+<meta http-equiv="refresh" content="3;url=http://userpages.umbc.edu/~justus2/handyman/home.html" />
+</head>
+<body background="bg.png">
+
+<div id="login">
+Welcome <?php echo $_POST["user"]; ?><br>
+<br>
+<br>
+Redirecting in 3 seconds...
+</div>
+</body>
+</html>
