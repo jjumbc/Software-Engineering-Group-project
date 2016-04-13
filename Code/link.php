3164f490b2d1cbb1d6e57080356df9bd8452047d
Matthew Wheeler <mwheel1@umbc.edu>
2016-04-13 19:30:03 +0000

    fixed DB name typo

--- Code/link.php
+++ Code/link.php
@@ -1,5 +1,5 @@
 <?php
-	$link = new mysqli("localhost", "root", "secret", "VerihandyDB");
+	$link = new mysqli("localhost", "root", "secret", "VeriHandyDB");
 	if (!$link) {
 		die("Connection failed: " . mysqli_connect_error());
 	}
