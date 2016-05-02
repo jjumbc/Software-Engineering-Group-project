4c62a696d27c42f81aee07cdf5a48b564316c885
Justus Jackson <justus2@umbc.edu>
2016-05-02 01:49:02 +0000

    edited headers to look nicer

--- Code/header.php
+++ Code/header.php
@@ -10,22 +10,21 @@
 		echo '<a href="viewprofile.php"><div id="linkcomp">User Profile</div></a>
 		<a href="joblist.php"><div id="linkcomp">Search Jobs</div></a>
 		<a href="createjob.php"><div id="linkcomp">Create New Job</div></a>
-		<a href="admin.php"><div id="linkcompadmin">Admin Panel</div></a>
-		<br>';
+		<a href="admin.php"><div id="linkcompadmin">Admin Panel</div></a>';
 		echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
 	}
 	else {
 		echo '<a href="viewprofile.php"><div id="link">User Profile</div></a>
 		<a href="joblist.php"><div id="link">Search Jobs</div></a>
-		<a href="createjob.php"><div id="link">Create New Job</div></a>
-		<br>';
+		<a href="createjob.php"><div id="link">Create New Job</div></a>';
 		echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
 	}
-
+	
+	echo '<br><div style="float: right; margin-right: 20px;">';
 	if (isset($_COOKIE["UserName"])) {
 		echo "Hello, ";
 		echo $_COOKIE["UserName"];
 		echo "! ";
 	}
-	echo '<a href="logout.php">Log Out</a></div></div><br><br>';
+	echo '<a href="logout.php">Log Out</a></div></div></div><br><br>';
 ?>
