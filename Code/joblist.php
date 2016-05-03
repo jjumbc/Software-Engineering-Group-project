b6f560b8b3ee2b5d3e2c18dec31ec9b7d0b403e3
Justus Jackson <actionjackson0@gmail.com>
2016-05-03 20:56:13 +0000

    fuck this

--- Code/joblist.php
+++ Code/joblist.php
@@ -36,22 +36,17 @@
 		$geocodeTo = file_get_contents('http://maps.google.com/maps/api/geocode/json?address='.$formattedAddrTo.'&sensor=false');
 		$outputTo = json_decode($geocodeTo);
 		
-		if (is_array($outputFrom->results) && is_array($outputTo->results)) {
-			$latitudeFrom = $outputFrom->results[0]->geometry->location->lat;
-			$longitudeFrom = $outputFrom->results[0]->geometry->location->lng;
-			$latitudeTo = $outputTo->results[0]->geometry->location->lat;
-			$longitudeTo = $outputTo->results[0]->geometry->location->lng;
-			
-			$theta = $longitudeFrom - $longitudeTo;
-			$dist = sin(deg2rad($latitudeFrom)) * sin(deg2rad($latitudeTo)) +  cos(deg2rad($latitudeFrom)) * cos(deg2rad($latitudeTo)) * cos(deg2rad($theta));
-			$dist = acos($dist);
-			$dist = rad2deg($dist);
-			$miles = $dist * 60 * 1.1515;
-			return $miles;
-		}
-		else {
-			return 0;
-		}
+		$latitudeFrom = $outputFrom->results[0]->geometry->location->lat;
+		$longitudeFrom = $outputFrom->results[0]->geometry->location->lng;
+		$latitudeTo = $outputTo->results[0]->geometry->location->lat;
+		$longitudeTo = $outputTo->results[0]->geometry->location->lng;
+		
+		$theta = $longitudeFrom - $longitudeTo;
+		$dist = sin(deg2rad($latitudeFrom)) * sin(deg2rad($latitudeTo)) +  cos(deg2rad($latitudeFrom)) * cos(deg2rad($latitudeTo)) * cos(deg2rad($theta));
+		$dist = acos($dist);
+		$dist = rad2deg($dist);
+		$miles = $dist * 60 * 1.1515;
+		return $miles;
 }
 ?>
 
